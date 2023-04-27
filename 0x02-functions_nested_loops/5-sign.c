c276b209eec5666a9b8a7425490ca96d33cd08bd
nouhabm <bnouhapro@gmail.com>
2023-04-26 17:20:03 -0700

    nuha3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,35 +1,28 @@
-#include "main.h"
+# include "main.h"
+/**
+ * print_sign-check the sign of a number.
+ *
+ * @n:Number whose sign is to be checked.
+ * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
+ */
 
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
+
+int print_sign(int n)
+{
+	if (n > 0)
 	{
-	    int r;
-
-
-	    r = print_sign(98);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(0);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(0xff);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(-1);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    return (0);
+		_putchar('+');
+		return (1);
 	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
